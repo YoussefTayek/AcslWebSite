b2bd62337a7a7710dd8ae0803cf9bccdf2662662
Youssef TAYEK <yousseftayek@gmail.com>
2021-01-24 19:01:57 +0100

    update site

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const idenACSL = document.querySelector('#idenacsl');
+
+const html = `<div class="mt-3">
+                <p>Tel: 0485615105 | 0487118606</p>
+                <p>Adresse: Seraing, Liège</p>
+              </div>`;
+idenACSL.innerHTML = html;
+
+
+
